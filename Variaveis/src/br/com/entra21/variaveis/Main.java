fecaa94362b6dd0ad9f7d973da72f699b8cae64c
wellitonborges <welliton_borges@hotmail.com>
2022-04-26 20:14:49 -0300

    NEw

---
+++ Variaveis/src/br/com/entra21/variaveis/Main.java
@@ -0,0 +1,48 @@
+package br.com.entra21.variaveis;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		byte idade;
+		idade = 30;
+
+		byte Outraidade = 31;
+
+		short ano = 2022;
+
+		short anoPassado;
+
+		anoPassado = 2021;
+
+		System.out.println("Minha idade é " + (idade + Outraidade));
+
+		System.out.println("Estamos em " + ano + " e ano passado foi " + anoPassado + ".");
+
+		int habitantesBrasil = 212060000;
+
+		System.out.println("No Brasil em " + ano + " temos exatamente " + habitantesBrasil + " habitantes");
+		
+		long habitanteMundoTerra1500;
+		habitanteMundoTerra1500=7900000;
+		
+		double habitantesAtualmente =790000000.00;
+		
+		float peso =70.5f, altura=1.72f;
+		
+		System.out.println("meu peso é "+peso+ " Minha altura é "+altura+".");
+		
+		char letra='R';
+		
+		boolean IntervaloAntesdasOito=false;
+		boolean IntervaloAgora=true;
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
