06da968b7017d2d09b5fd1bcbf355ccbd065a1d8
wellitonborges <welliton_borges@hotmail.com>
2022-04-26 21:10:08 -0300

    new

--- Variaveis/src/br/com/entra21/variaveis/Main.java
+++ Variaveis/src/br/com/entra21/variaveis/Main.java
@@ -1,5 +1,7 @@
 package br.com.entra21.variaveis;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
@@ -36,6 +38,39 @@ public class Main {
 		
 		boolean IntervaloAntesdasOito=false;
 		boolean IntervaloAgora=true;
+		
+		String nomeCompleto="";
+		
+		String nome;
+		byte minhaIdade;
+		float minhaaltura,salario;
+		
+		
+		Scanner entrada;
+		entrada = new Scanner(System.in);
+		
+		System.out.println("Por Favor qual é o seu nome?");
+		nome = entrada.next();
+		
+		System.out.println("Me informe sua idade: ");
+		minhaIdade = entrada.nextByte();
+		
+		
+		System.out.println("Bem Vindo "+nome+" Agora consigo interagir com usuario");
+		System.out.println("Agora ja sei que você tem "+minhaIdade+" Anos de idade.");
+		
+     	System.out.println(nome+" Para melhor atender preciso saber qual é sua altura");
+		altura = entrada.nextFloat();
+		
+		System.out.println("Então "+nome+" você tem"+minhaIdade+ " e você tem "+altura+" de altura.");
+		
+		System.out.println("Qual é seu sdalario que pretende Ganhar Programando?");
+		salario = entrada.nextFloat();
+		
+		System.out.println("Então "+nome+ " No Momento Não OCnseguimos Pagar o salario de "+salario+" Para alguem que não sabe programar.");
+		System.out.println("Tente aprender mais para conseguirmos Pagar melhor Para você");
+		System.out.println("Muito Obrigado bom Aporendizado "+nome);
+		
 		
 		
 		
